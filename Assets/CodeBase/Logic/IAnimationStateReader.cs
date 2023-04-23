12bbccc98528302d2cd097d96c294b93f3b4b17a
Roman Kopylov <ze6est1@yandex.ru>
2023-04-23 16:17:54 +0300

    Added EnenyAnimator

---
+++ Assets/CodeBase/Logic/IAnimationStateReader.cs
@@ -0,0 +1,9 @@
+﻿namespace CodeBase.Logic
+{
+    public interface IAnimationStateReader
+    {
+        public void EnteredState(int stateHash);
+        public void ExitedState(int stateHash);
+        public AnimatorState State { get; }
+    }
+}
