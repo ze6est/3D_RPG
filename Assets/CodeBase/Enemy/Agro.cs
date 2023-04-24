b6558099b37a982f2b650830566abd2162962b13
Roman Kopylov <ze6est1@yandex.ru>
2023-04-24 20:40:29 +0300

    Added Agro

---
+++ Assets/CodeBase/Enemy/Agro.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace CodeBase.Enemy
+{
+    public class Agro : MonoBehaviour
+    {
+        [SerializeField] private TriggerObserver _triggerObserver;
+        [SerializeField] private AgentMoveToPlayer _follow;
+
+        private void Start()
+        {
+            _triggerObserver.TriggerEnter += OnTriggerEnter;
+            _triggerObserver.TriggerExit += OnTriggerExit;
+
+            SwitchFollowOff();
+        }
+
+        private void OnTriggerExit(Collider obj)
+        {
+            SwitchFollowOff();
+        }
+
+        private void OnTriggerEnter(Collider obj)
+        {
+            SwitchFollowOn();
+        }
+
+        private void SwitchFollowOn()
+        {
+            _follow.enabled = true;
+        }
+
+        private void SwitchFollowOff()
+        {
+            _follow.enabled = false;
+        }
+    }
+}
