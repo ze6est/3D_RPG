d174151d469da9f98e3c5d8a8acbd56c4088e9f6
Roman Kopylov <ze6est1@yandex.ru>
2023-05-02 21:21:58 +0300

    Created PlayerDeath and effect die

---
+++ Assets/CodeBase/Player/PlayerDeath.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace CodeBase.Player
+{
+    public class PlayerDeath : MonoBehaviour
+    {
+        [SerializeField] private PlayerHealth _health;
+        [SerializeField] private PlayerAnimator _animator;
+        [SerializeField] private PlayerMove _move;
+        [SerializeField] private GameObject _deathFx;
+
+        private bool _isDeath;
+
+        private void Start()
+        {
+            _health.HealthChanged += OnHealthChanged;
+        }
+
+        private void OnDestroy()
+        {
+            _health.HealthChanged -= OnHealthChanged;
+        }
+
+        private void OnHealthChanged()
+        {
+            if (!_isDeath && _health.Current <= 0)
+                Die();
+        }
+
+        private void Die()
+        {
+            _isDeath = true;
+
+            _move.enabled = false;
+            _animator.PlayDeath();
+
+            Instantiate(_deathFx, transform.position, Quaternion.identity);
+        }
+    }
+}
