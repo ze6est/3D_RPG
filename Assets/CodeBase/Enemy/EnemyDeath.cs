612685b0bed309de740fb0848ee4b239ce025f49
Roman Kopylov <ze6est1@yandex.ru>
2023-05-10 12:48:27 +0300

    Created EnemyDeath

---
+++ Assets/CodeBase/Enemy/EnemyDeath.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+namespace CodeBase.Enemy
+{
+    public class EnemyDeath : MonoBehaviour
+    {
+        private const float Delay = 5f;
+
+        [SerializeField] private EnemyAnimator _animator;
+        [SerializeField] private EnemyHealth _health;
+        [SerializeField] private GameObject _deathFx;
+
+        public event Action DeathChanged;
+
+        private void Start()
+        {
+            _health.HealthChanged += OnHealthChanged;
+        }
+
+        private void OnDestroy()
+        {
+            _health.HealthChanged -= OnHealthChanged;
+        }
+
+        private void OnHealthChanged()
+        {
+            if (_health.Current <= 0)
+                Die();
+        }
+
+        private void Die()
+        {
+            _health.HealthChanged -= OnHealthChanged;
+
+            _animator.PlayDeath();
+            SpawnDeathFx();
+
+            DeathChanged?.Invoke();
+
+            Destroy(gameObject, Delay);
+        }
+
+        private void SpawnDeathFx()
+        {
+            Instantiate(_deathFx, transform.position, Quaternion.identity);
+        }
+    }
+}
