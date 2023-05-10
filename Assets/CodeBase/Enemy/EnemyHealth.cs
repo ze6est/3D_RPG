f818b0c20ab7fb01638a8c9f0ec550d65fefd799
Roman Kopylov <ze6est1@yandex.ru>
2023-05-10 12:18:02 +0300

    Created EnemyHealth

---
+++ Assets/CodeBase/Enemy/EnemyHealth.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace CodeBase.Enemy
+{
+    public class EnemyHealth : MonoBehaviour
+    {
+        [SerializeField] private EnemyAnimator _animator;
+
+        private float _current;
+        private float _max;
+
+        public event Action HealthChanged;
+
+        public void TakeDamage(float damage)
+        {
+            _current -= damage;
+
+            HealthChanged?.Invoke();
+
+            _animator.PlayHit();
+        }
+        
+    }
+}
